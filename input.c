6b8f17a0398855552b0af3e2c6296d79cd385981
ABDULLAH BAGHUTH <36686812+abdullah-baghuth@users.noreply.github.com>
2020-03-29 20:15:58 -0700

    Add files via upload

---
+++ input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+
+	int a;
+	printf("Enter the value of a:\n");
+	scanf("%d",&a);
+	printf("the value of a is %d\n",a);
+return 0;
+}
---
+++ output.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+
+	int a;
+	writef("Enter the value of a:\n");
+	readf("%d",&a);
+	writef("the value of a is %d\n",a);
+return 0;
+}
